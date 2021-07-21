0c69a03fe552200708d2d8a2b3e5b27d0c068a2a
Siwa Khongsuphap <siwa@hospital-os.com>
2021-07-21 22:49:42 +0700

    add pattern19 alternative

---
+++ pattern19-2.js
@@ -0,0 +1,29 @@
+const n = 5;
+
+for (let i = 1; i <= n; i++) {
+  let str = "";
+
+  for (let j = 1; j <= i; j++) {
+    str += " ";
+  }
+
+  for (let k = i; k <= n; k++) {
+    str += "* ";
+  }
+
+  console.log(str);
+}
+
+for (let i = n; i >= 1; i--) {
+  let str = "";
+
+  for (let j = 1; j <= i; j++) {
+    str += " ";
+  }
+
+  for (let k = i; k <= n; k++) {
+    str += "* ";
+  }
+
+  console.log(str);
+}
