f017cf5c1e6e516f5cbf0deac0384a1a3fceff7d
Siwa Khongsuphap <siwa@hospital-os.com>
2021-08-08 13:14:15 +0700

    add spiral pattern 3

---
+++ spiral_pattern3.js
@@ -0,0 +1,86 @@
+/*
+
+21 20 19 18 17 
+22 7  6  5  16 
+23 8  1  4  15 
+24 9  2  3  14 
+25 10 11 12 13
+
+*/
+
+function printSpiral(n) {
+  let divideN = Math.floor(n / 2);
+
+  let col = n % 2 === 0 ? divideN - 1 : divideN;
+  let row = n % 2 === 0 ? divideN - 1 : divideN;
+  let boundary = 1;
+  let sizeLeft = 1;
+  let flag = 2;
+  let move = "d";
+  let matrix = new Array(n);
+
+  for (let i = 0; i < n; i++) {
+    matrix[i] = new Array(n).fill(0);
+  }
+
+  for (let i = 1; i <= n * n; i++) {
+    matrix[row][col] = i;
+
+    switch (move) {
+      case "d":
+        row += 1;
+        break;
+      case "r":
+        col += 1;
+        break;
+      case "u":
+        row -= 1;
+        break;
+      case "l":
+        col -= 1;
+        break;
+    }
+
+    if (i === boundary) {
+      boundary += sizeLeft;
+
+      if (flag !== 2) {
+        flag = 2;
+      } else {
+        flag = 1;
+        sizeLeft += 1;
+      }
+
+      switch (move) {
+        case "d":
+          move = "r";
+          break;
+        case "r":
+          move = "u";
+          break;
+        case "u":
+          move = "l";
+          break;
+        case "l":
+          move = "d";
+          break;
+      }
+    }
+  }
+
+  let countNPowerDigit = (n * n).toString().length;
+
+  for (let i = 0; i < n; i++) {
+    let str = "";
+
+    for (let j = 0; j < n; j++) {
+      let currentValue = matrix[i][j];
+      str +=
+        `${currentValue} ` +
+        " ".repeat(countNPowerDigit - currentValue.toString().length);
+    }
+    console.log(str);
+  }
+}
+
+printSpiral(5);
